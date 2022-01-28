caf3a77a5cba0df1493dfa81535dd2afb2dc808f
Pierre Monchecourt <monchecour@eisti.eu>
2022-01-28 14:53:59 +0100

    Added functions for exercise 3

--- td1/src/main/scala/expression/Expression.scala
+++ td1/src/main/scala/expression/Expression.scala
@@ -1,5 +1,39 @@
 package expression
 
-class Expression {
+import rational.Rational
+
+/**
+ * Represents Expression
+ *
+ * @note We add 'sealed' because we want to specify to Scala that there will not exist any other under-class outside this file
+ * @note Each class is a case class because we want to make pattern matching
+ *
+ */
+sealed trait Expression {
+  val eval: Rational = this match {
+    case Constant(x) => x
+    case Plus(e1, e2) => e1.eval + e2.eval
+    case Minus(e1, e2) => e1.eval - e2.eval
+    case Times(e1, e2) => e1.eval * e2.eval
+    case Opposite(e) => -e.eval
+  }
+
+  def +(that: Expression) = Plus(this, that)
+
+  def -(that: Expression) = Minus(this, that)
+
+  def *(that: Expression) = Times(this, that)
+
+  lazy val unary_- = Opposite(this)
 
 }
+
+case class Constant(x: Rational) extends Expression
+
+case class Plus(e1: Expression, e2: Expression) extends Expression
+
+case class Minus(e1: Expression, e2: Expression) extends Expression
+
+case class Times(e1: Expression, e2: Expression) extends Expression
+
+case class Opposite(e: Expression) extends Expression
